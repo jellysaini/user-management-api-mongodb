8a56ce894280f3cb40784941d2f9a4173d2d1c2f
jelly saini <jellysaini.1987@gmail.com>
2024-10-02 14:41:04 +0530

    add the code

---
+++ UserManagementAPI/Controllers/UserController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using UserManagementAPI.Model;
+using UserManagementAPI.Services;
+
+namespace UserManagementAPI.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class UserController :ControllerBase
+    {
+        private readonly UserService _userService;
+
+        public UserController(UserService userService)
+        {
+            _userService = userService;
+        }
+
+        [HttpGet]
+        public ActionResult<List<User>> GetAll() => _userService.GetAll();
+
+        [HttpPost]
+        public ActionResult<User> Create(User user)
+        {
+            if (ModelState.IsValid)
+            {
+                _userService.Create(user);
+                return Ok(user);
+            }
+            return BadRequest();
+        }
+    }
+}
---
+++ UserManagementAPI/Services/UserService.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.Extensions.Options;
+using MongoDB.Driver;
+using UserManagementAPI.Model;
+
+namespace UserManagementAPI.Services
+{
+    public class UserService
+    {
+        private readonly IMongoCollection<User> _users;
+
+        public UserService(IOptions<MongoDbSettings> mongoDbSettings)
+        {
+            var client = new MongoClient(mongoDbSettings.Value.ConnectionString);
+            var database = client.GetDatabase(mongoDbSettings.Value.Database);
+            _users = database.GetCollection<User>("Users");
+
+            // Create compound index for GDPR compliance (e.g., unique email)
+            var indexKeys = Builders<User>.IndexKeys.Ascending(user => user.Email);
+            _users.Indexes.CreateOne(new CreateIndexModel<User>(indexKeys));
+        }
+
+        public List<User> GetAll() => _users.Find(user => true).ToList();
+
+        public User Get(int id) => _users.Find(user => user.Id == id).FirstOrDefault();
+
+        public User Create(User user)
+        {
+            user.CreatedAt = DateTime.UtcNow;
+            _users.InsertOne(user);
+            return user;
+        }
+    }
+}
